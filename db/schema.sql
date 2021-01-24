6201f6f3df3135c665d9f045e31968fc4a5785c8
Rowan Oliver <r.oliver.yoga@gmail.com>
2021-01-23 22:11:10 -0800

    starting framework

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE burger_db; 
+
+CREATE TABLE  burgers (
+    id (30) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR 
+    
+    PRIMARY KEY(id)
+    
+);
+   
+    
+
