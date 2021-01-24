b0f0047127a4589a8575bd3cc6e309f369d3ad55
Rowan Oliver <r.oliver.yoga@gmail.com>
2021-01-24 00:33:26 -0800

    getting error

--- models/burger.js
+++ models/burger.js
@@ -8,5 +8,25 @@ var burger = {
             callback(res);
         });
     },
+    //CRUD 
+    insertOne: function(vals, callback) {
+        orm.insertOne("burgers". "burger_name", vals, function(res){
+            console.log("Inserted a burger to be devoured", vals)
+            callback(res);
+        })
+    },
+
+    //update 
+    updateOne: function(vals, callback) {
+        orm.updateOne(colVal, id, function(res){
+            callback(res)
+        });
+    }
+    //delete, figure out error 
+    deleteOne: function(id, callback) {
+        orm.deleteOne(id, function(res){
+            callback(res);
+        });
+    }
 
 }
