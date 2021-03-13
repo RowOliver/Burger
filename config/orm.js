66f5911bc4fd67f4dbebd0693b772ce5bfe5e30a
Rowan Oliver <r.oliver.yoga@gmail.com>
2021-03-12 23:39:29 -0800

    middleware issues

--- config/orm.js
+++ config/orm.js
@@ -3,14 +3,41 @@
 
 var connection = require("./connection");
 
-//Create fucnction 
-//Update function 
-// Delete function 
+var orm = {
+    selectAll: function(tabel,callback) {
+        var queryString = " SELECT * FROM ??;";
+        connection.query(queryString, [table], function(err,result){
+            if (err) throw err;
+            callback(result);
+        });
+    },
+
+    insertOne: function(table, cols, vals, callback){
+        var queryString = "INSERT INTO ?? (??) VALUES (?);";
+        connection.query(queryString, [table, cols, vals], function(err, data){
+            if (err) throw err;
+            callback(data);
+        });
 
 
+    },
 
+    updateOne: function(colVal, id, callback) {
+        var queryString = "UPSTE burgers SET devoured='1' WHERE " + id + ";";
+        connection.query(queryString, [id], function(err, result){
+            if (err) throw err;
+            callback(result);
+        });
+    },
 
-
+    deleteOne: function(id, callback) {
+        var queryString = "DELETE FROM burger WHERE " + id + ";";
+        connection.query(queryString, [id], function(err, result){
+            if (err) throw err;
+            callback(result);
+        });
+    },
+}
 
 
 //last line of code 
